3389a58c75ba33ab4befbaf947a3c444ead89927
Rasoulmadani <ali.rahbary@chmail.ir>
2024-11-01 09:26:03 +0330

    قرار دادن اسپینر برای گرفتن داده ها

--- src/pages/users/index.vue
+++ src/pages/users/index.vue
@@ -2,23 +2,28 @@
 import axios from "axios";
 import {ref} from "vue";
 import UserCardView from '../../components/users/CardView.vue'
+
 export default {
   components: {
     UserCardView
   },
   setup() {
     const users = ref([]);
+    const loading = ref(true);
+
     function getUsers() {
       axios.get("https://jsonplaceholder.typicode.com/users")
           .then((response) => {
             users.value = response.data;
+            loading.value = false;
           })
           .catch((error) => {
             console.log(error);
           })
     }
+
     getUsers();
-    return{users}
+    return {users}
   }
 }
 </script>
@@ -26,10 +31,14 @@ export default {
 <template>
   <div class="container mt-5">
     <div class="row g-3">
-      <div class="col-md-4" v-for="user in users" :key="user.id">
 
-        <UserCardView :user="user"  />
-        </div>
+      <div v-if="loading" class="spinner-border" role="status">
+        <span class="sr-only"></span>
+      </div>
+
+      <div v-else class="col-md-4" v-for="user in users" :key="user.id">
+        <UserCardView :user="user"/>
+      </div>
     </div>
   </div>
 </template>
