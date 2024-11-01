5c1530dd4dbe8ab4cb669000f631f52e6fd6d8c3
Rasoulmadani <ali.rahbary@chmail.ir>
2024-11-01 08:37:01 +0330

    اضافه کردن استایل به هدر هایی که فعال هستند

--- src/components/sections/Header.vue
+++ src/components/sections/Header.vue
@@ -14,7 +14,7 @@ export default {
 
       <div class="collapse navbar-collapse" id="navbarSupportedContent">
         <ul class="navbar-nav mr-auto">
-          <li class="nav-item active">
+          <li class="nav-item">
             <router-link to="/" class="nav-link" href="#">Home</router-link>
           </li>
           <li class="nav-item">
@@ -32,5 +32,8 @@ export default {
 </template>
 
 <style scoped>
-
+.router-link-active{
+  color: rgba(0,0,0,0.9) !important;
+  border-bottom: 2px solid rgba(0,0,0,0.9) !important;
+}
 </style>
