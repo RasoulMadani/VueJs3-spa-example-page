5596fb24d6df90dca4080fcc5a96eb1a705ca143
Rasoulmadani <ali.rahbary@chmail.ir>
2024-11-01 08:47:58 +0330

    نصب اکسیوس و استفاده از آن

---
+++ src/pages/users/index.vue
@@ -0,0 +1,27 @@
+<script>
+import axios from "axios";
+
+export default {
+  setup() {
+
+    function getUsers() {
+      axios.get("https://jsonplaceholder.typicode.com/users")
+          .then((response) => {
+            console.log(response.data);
+          })
+          .catch((error) => {
+            console.log(error);
+          })
+    }
+    getUsers();
+  }
+}
+</script>
+
+<template>
+  <h1>users</h1>
+</template>
+
+<style scoped>
+
+</style>
