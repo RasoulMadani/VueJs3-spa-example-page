3b71a897aa1f9fbb41ffc5a6684ecf2e5ec50e60
Rasoulmadani <ali.rahbary@chmail.ir>
2024-11-01 13:07:46 +0330

    قرار دادن دکمه حذف و ویرایش

--- src/pages/posts/Show.vue
+++ src/pages/posts/Show.vue
@@ -9,7 +9,7 @@ export default {
     const loading = ref(true);
     const route = useRoute();
     function getPost() {
-      axios.get(` https://jsonplaceholder.typicode.com/posts/${route.params.id}` )
+      axios.get(` https://jsonplaceholder.typicode.com/posts/${route.params.id}`)
           .then((response) => {
             console.log(response.data);
             post.value = response.data;
@@ -22,7 +22,7 @@ export default {
     }
 
     getPost();
-    return {post,loading,route}
+    return {post, loading, route}
   }
 }
 </script>
@@ -30,23 +30,27 @@ export default {
 <template>
 
 
-      <div v-if="loading" class="spinner-border" role="status">
-        <span class="sr-only"></span>
+  <div v-if="loading" class="spinner-border" role="status">
+    <span class="sr-only"></span>
+  </div>
+
+  <div v-else class="col-md-6">
+    <div class="card">
+      <div class="card-header">
+        {{ post.title }}
       </div>
 
-      <div v-else class="col-md-6">
-        <div class="card" >
-          <div class="card-header">
-            {{ post.title }}
-          </div>
 
+      <ul class="list-group list-group-flush">
+        <li class="list-group-item">Username : {{ post.body }}</li>
 
-          <ul class="list-group list-group-flush">
-            <li class="list-group-item">Username : {{post.body}}</li>
-
-          </ul>
-        </div>
+      </ul>
+      <div class="card-footer">
+        <button class="btn btn-sm btn-danger me-4">Delete</button>
+        <button class="btn btn-sm btn-dark">Edit</button>
       </div>
+    </div>
+  </div>
 
 
 </template>
