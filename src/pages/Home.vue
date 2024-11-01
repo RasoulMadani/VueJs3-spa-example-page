8f4b43a8f08d1cbaf82a85c0eb94f0ca6811b119
Rasoulmadani <ali.rahbary@chmail.ir>
2024-11-01 10:30:57 +0330

    ایجاد یک دکمه تا با زدن روی آن به صفحه مورد نظر برود

--- src/pages/Home.vue
+++ src/pages/Home.vue
@@ -7,6 +7,7 @@
   <div class="row">
     <div class="col-md-6">
       <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. A commodi consequatur cumque dignissimos dolore eaque esse eveniet exercitationem expedita fugit, illo maiores mollitia nostrum optio perferendis porro quam rerum ut voluptate voluptatem. Aliquam cumque dolor labore obcaecati quis, soluta temporibus?</p>
+      <router-link class="btn btn-dark" :to="{ name: 'users' }">Users</router-link>
     </div>
   </div>
 </div>
